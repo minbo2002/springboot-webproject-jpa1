db77fdc5b50faedf9661862cbf9b41cc326e94de
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-07-07 02:47:45 +0900

    1. 홈화면을 위한 HomeController 생성
    2. 레이아웃 설정(home.html , header.html , footer.html , bodyHeader.html)
    
    3. 부트스트랩 적용

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -54,7 +54,7 @@ public class OrderRepository {
             } else {
                 jpql += " and";
             }
-            jpql += " m.name like :name"
+            jpql += " m.name like :name";
         }
 
         TypedQuery<Order> query = em.createQuery(jpql, Order.class)
