748215e6c631fd52b0725d1e80bb48bb3d16008b
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-07-07 13:44:52 +0900

    1. 회원등록을 위한 MemberForm, MemberController 생성
    2. 회원등록 화면 생성

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원등록 화면만 보여주는곳
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+    // 회원등록 데이터를 실제로 등록하는곳
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());  // 입력한 name을 넣는다
+        member.setAddress(address);  // 입력한 city, street, zipcode를 넣는다
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
