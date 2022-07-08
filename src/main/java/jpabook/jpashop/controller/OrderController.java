a6d82189b157c12d2ce38ebc3bc045dc8a0ef625
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-07-08 10:42:14 +0900

    1. 주문, 주문검색, 주문내역을 위한 OrderController 생성
    2. 주문화면 생성
    
    3. 주문내역 화면 생성

---
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,69 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    // 주문 화면만 보여주는곳
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    // 주문 데이터를 파싱하는곳
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+
+        orderService.order(memberId, itemId, count);
+
+        return "redirect:/orders";
+    }
+
+    // 검색
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch")OrderSearch orderSearch, Model model) {
+
+        List<Order> orders = orderService.findOrders(orderSearch);
+
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    // 주문취소
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+
+        orderService.cancelOrder(orderId);
+
+        return "redirect:/orders";
+    }
+}
