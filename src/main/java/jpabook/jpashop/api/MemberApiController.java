5809563c37f0ab5e9b5a5f35de3a6e0114cd13b8
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-07-10 04:31:09 +0900

    1. 엔티티를 외부에 노출시키지 않기위해 별도의 dto 클래스 생성

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 @RestController  //@Controller + @ResponseBody
 @RequiredArgsConstructor
@@ -16,7 +17,7 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    // 회원등록 api
+    // 회원등록 (엔티티 변경하면 api 스펙 바뀜)
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
 
@@ -24,6 +25,26 @@ public class MemberApiController {
 
         return new CreateMemberResponse(id);
     }
+
+    // 회원등록 (엔티티 변경하면 api 스펙 안바뀐다 + 엔티티를 외부에 노출하지 않는다)
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+        member.setName(request.name);
+
+        Long id = memberService.join(member);
+
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+
+        @NotEmpty
+        private String name;
+    }
+
 
     @Data  // 등록된 Id를 반환하는 용도
     static class CreateMemberResponse {
